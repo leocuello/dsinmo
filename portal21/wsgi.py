e10c54622ae5334d6efc66de2a692c0616bc82d3
Leo Cuello <leocuello@gmail.com>
2019-09-15 20:57:14 -0300

    lang

--- portal21/wsgi.py
+++ portal21/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal21.settings')
 
 application = get_wsgi_application()
+
+django_heroku.settings(locals())
